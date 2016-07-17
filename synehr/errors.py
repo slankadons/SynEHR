166556b9e2ed303646bfec3a5b860ca0dbb0daec
slankadons <slanka@dons.usfca.edu>
2016-07-17 12:57:58 -0700

    Update errors.py

--- synehr/errors.py
+++ synehr/errors.py
@@ -30,6 +30,32 @@ def gen_errors(size,data):
             errors[item]+=rem
     #print errors
     #print sum(errors.values())
+
+    # Insertion Code
+    insertion_index = np.random.randint(low=0,high=len(data), size=errors['insertion'])
+    insertion_data = data.iloc[insertion_index]
+    insertion_err={'first':0,'last':0,'DOB':0,'address':0}
+    val = sum_num_terms_equals_total(4, len(insertion_data))
+    # print val
+    i = 0
+    for item in insertion_err:
+        insertion_err[item] = int(val[i])
+        i += 1
+    insertion_first = insertion_data.sample(n=insertion_err['first'])
+    col_first = insertion_first['first'].tolist()
+    insertion_first['first'] = char_insertion_str(col_first)
+
+    insertion_last = insertion_data.sample(n=insertion_err['last'])
+    col_last = insertion_last['last'].tolist()
+    insertion_last['last'] = char_insertion_str(col_last)
+
+    insertion_addr = insertion_data.sample(n=insertion_err['address'])
+    col_addr = insertion_addr['address'].tolist()
+    insertion_addr['address'] = char_insertion_str(col_addr)
+
+    insertion_err = pd.concat([insertion_first, insertion_last, insertion_addr])
+
+
     gender_mis_index=np.random.randint(low=0,high=len(data),size=errors['gender_mis'])
     gender_mis_data=data.iloc[gender_mis_index]
     gender_mis_error=gender_misclassification(gender_mis_data)
@@ -113,7 +139,11 @@ def gen_errors(size,data):
 
     transpo_err = pd.concat([sub_first, sub_last, sub_addr, sub_DOB])
 
-    data_err=pd.concat([gender_mis_error,omission_err,sub_err,transpo_err])
+
+    data_err=pd.concat([insertion_err,gender_mis_error,omission_err,sub_err,transpo_err])
+
+
+
     return data_err
 
 def gender_misclassification(data):
@@ -128,6 +158,7 @@ def gender_misclassification(data):
     data['gender'].replace('O', 'F', inplace=True)
     #print "after swap: ",data['gender']
     #return data
+
 
 def char_sub_str(arr):
     """
@@ -151,6 +182,8 @@ def char_sub_str(arr):
             x = x[0:rnd] + rndCharacter + x[rnd + 1:]
             arr[i] = x
     return arr
+
+
 
 
 def char_omission(col_data):
@@ -178,6 +211,31 @@ def char_omission(col_data):
         col_data[i]=c
         #print col_data[i]
     return col_data
+
+
+def char_sub_str(arr):
+    """
+    Generates characters substitution error in string
+    :param Data:
+    :return:
+    """
+    characters = 'qwertyuioplkjhgfdsazxcvbnm'
+
+    for i in range(0, len(arr)):
+        x = arr[i]
+        # print x
+        # print len(x)
+        if len(x) > 3:
+            rnd = random.randint(2, len(x) - 2)
+            # print rnd
+            tmp1 = random.randint(0, len(characters))
+            rndCharacter = characters[tmp1:tmp1 + 1]
+            # print rndCharacter
+            # x[rnd:rnd+1] = rndCharacter
+            x = x[0:rnd] + rndCharacter + x[rnd + 1:]
+            arr[i] = x
+    return arr
+
 
 def char_sub_date(col_date):
     """
@@ -282,6 +340,32 @@ def char_transpo_date(col_date):
         new_dates += [date]
         # print new_dates
     return new_dates
+
+
+
+def char_insertion_str(arr):
+    """
+    Generates characters substitution error in string
+    :param Data:
+    :return:
+    """
+    characters = 'qwertyuioplkjhgfdsazxcvbnm'
+    print "insertion before", arr
+    for i in range(0, len(arr)):
+        x = arr[i]
+        # print x
+        # print len(x)
+        if len(x) > 3:
+            rnd = random.randint(2, len(x) - 2)
+            # print rnd
+            tmp1 = random.randint(0, len(characters))
+            rndCharacter = characters[tmp1:tmp1 + 1]
+            # print rndCharacter
+            # x[rnd:rnd+1] = rndCharacter
+            x = x[0:rnd] + rndCharacter + x[rnd:]
+            arr[i] = x
+    print "insertion after: ", arr
+    return arr
 
 
 
