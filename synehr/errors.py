6af365abd66b8cc167771d7386d41f4d4dce60c1
slankadons <slanka@dons.usfca.edu>
2016-07-17 11:15:38 -0700

    Character Transposition Error

--- synehr/errors.py
+++ synehr/errors.py
@@ -61,7 +61,7 @@ def gen_errors(size,data):
     sub_DOB['DOB']=char_sub_date(col_DOB)
     print "date error: ",sub_DOB.head()
 
-    sub_err = pd.concat([sub_first, sub_last, sub_addr])
+    sub_err = pd.concat([sub_first, sub_last, sub_addr,sub_DOB])
 
     char_omission_index=np.random.randint(low=0,high=len(data),size=errors['omission'])
     char_omission_data=data.iloc[char_omission_index]
@@ -84,9 +84,36 @@ def gen_errors(size,data):
     omission_addr['address']=char_omission(col_addr)
     omission_err=pd.concat([omission_first,omission_last,omission_addr])
 
+    char_transpo_index = np.random.randint(low=0, high=len(data), size=errors['transposition'])
+    char_transpo_data = data.iloc[char_transpo_index]
+    transpo_err = {'first': 0, 'last': 0, 'DOB': 0, 'address': 0}
+    val = sum_num_terms_equals_total(4, len(char_transpo_data))
+    # print val
+    i = 0
+    for item in transpo_err:
+        transpo_err[item] = int(val[i])
+        i += 1
+    transpo_first = char_transpo_data.sample(n=transpo_err['first'])
+    col_first = transpo_first['first'].tolist()
+    transpo_first['first'] = char_transpo_str(col_first)
+    # print omission_first['first']
 
+    transpo_last = char_transpo_data.sample(n=transpo_err['last'])
+    col_last = transpo_last['last'].tolist()
+    transpo_last['last'] = char_transpo_str(col_last)
 
-    data_err=pd.concat([gender_mis_error,omission_err,sub_err])
+    transpo_addr = char_transpo_data.sample(n=transpo_err['address'])
+    col_addr = transpo_addr['address'].tolist()
+    transpo_addr['address'] = char_transpo_str(col_addr)
+
+    transpo_DOB = char_transpo_data.sample(n=transpo_err['DOB'])
+    col_DOB = transpo_DOB['DOB'].tolist()
+    transpo_DOB['DOB'] = char_transpo_date(col_DOB)
+    print "date error: ", sub_DOB.head()
+
+    transpo_err = pd.concat([sub_first, sub_last, sub_addr, sub_DOB])
+
+    data_err=pd.concat([gender_mis_error,omission_err,sub_err,transpo_err])
     return data_err
 
 def gender_misclassification(data):
@@ -102,26 +129,6 @@ def gender_misclassification(data):
     #print "after swap: ",data['gender']
     #return data
 
-def character_substitution(data):
-    """
-    Function to induce character substitution error
-    :param data: pandas dataframe
-    :return: data: pandas dataframe
-    """
-    charactersalpha = 'qwertyuioplkjhgfdsazxcvbnm'
-    charnum = '1234567890'
-    error_data={'first':0,'last':0,'DOB':0,'address':0}
-    val=sum_num_terms_equals_total(4,len(data))
-    i=0
-    for item in error_data:
-        error_data[item]=val[i]
-        i+=1
-    sub_first=data.sample(n=error_data['first'])
-    sub_first
-
-
-    return
-
 def char_sub_str(arr):
     """
     Generates characters substitution error in string
@@ -135,15 +142,14 @@ def char_sub_str(arr):
         # print x
         # print len(x)
         if len(x) > 3:
-            if (random.randint(0, 2) == 1):
-                rnd = random.randint(2, len(x) - 2)
+            rnd = random.randint(2, len(x) - 2)
                 # print rnd
-                tmp1 = random.randint(0, len(characters))
-                rndCharacter = characters[tmp1:tmp1 + 1]
+            tmp1 = random.randint(0, len(characters))
+            rndCharacter = characters[tmp1:tmp1 + 1]
                 # print rndCharacter
                 # x[rnd:rnd+1] = rndCharacter
-                x = x[0:rnd] + rndCharacter + x[rnd + 1:]
-                arr[i] = x
+            x = x[0:rnd] + rndCharacter + x[rnd + 1:]
+            arr[i] = x
     return arr
 
 
@@ -210,6 +216,73 @@ def char_sub_date(col_date):
         date=datetime.datetime.strptime(date_err,"%Y/%m/%d")
         new_dates+=[date]
     return new_dates
+
+def char_transpo_str(col_data):
+    """
+    Function to induce character transposition errors
+    :param col_data: list, column to induce errors in.
+    :return: col_data
+    """
+    new_col=[]
+    #print col_data
+    for record in col_data:
+        print record
+        if len(record)>3:
+            rnd = random.randint(2,len(record)-2)
+            # print rnd
+            #random.choice(range(1,len(record)))
+            ind=random.choice([1,-1])
+            recordl=list(record)
+            buf=recordl[rnd]
+            recordl[rnd]=record[rnd+ind]
+            recordl[rnd+ind]=buf
+            record=''.join(recordl)
+        new_col+=[record]
+
+    return new_col
+
+def char_transpo_date(col_date):
+    """
+    Function to randomly transpose date characters
+    :param col_date: list of dates
+    :return: new_date: list of error induced dates
+    """
+    new_dates = []
+    print col_date
+    for date in col_date:
+        date = date.strftime('%Y/%m/%d')
+        print "Date before error: ",date
+        year, month, day = date.split('/')
+        # year=int(year[-2:])
+        month = (month)
+        day = (day)
+        ind = [0,2]
+        choice = random.choice(ind)
+        if choice == 0:
+
+            year = list(year)
+            if abs(int(year[2])-int(year[3]))==1 or random.randint(0,10)==1:
+                buf=year[3]
+                year[3]=year[2]
+                year[2]=buf
+            year = ''.join(year)
+            print "Error induced year: ",year
+
+        elif choice == 2:
+            dayl=list(day)
+            if len(day)==1 and int(day)<=2:
+                day*=10
+            elif abs(int(dayl[0])-int(dayl[1]))==1 or random.randint(0,10)==1:
+                buf=dayl[0]
+                dayl[0]=dayl[1]
+                dayl[1]=buf
+                day=''.join(dayl)
+        date_err = '/'.join([year, month, day])
+        date = datetime.datetime.strptime(date_err, "%Y/%m/%d")
+        new_dates += [date]
+        print new_dates
+    return new_dates
+
 
 
 
