0667f7f33202eed7c4fa940893e61b6ff6a49b21
Tennyson Lee <tennysonlee@tleeMBA.local>
2016-08-09 22:22:08 -0700

    setup.py change

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='synehr',
       version='0.1',
       description='Package to generate synthetic identified EHR data',
-      url='http://github.com/storborg/funniest',
+      url='https://github.com/slankadons/SynEHR/tree/type_gen_pull',
       author='Shraddha Lanka, Tennyson Lee',
       author_email='slanka@dons.usfca.edu, talee@dons.usfca.edu',
       license='',
